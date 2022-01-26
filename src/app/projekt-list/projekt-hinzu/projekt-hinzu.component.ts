4cec07e433b05052ee55cf71dfd08f1aff6b77be
Thomas Haselwander <thomas.haselwander@gmail.com>
2022-01-26 22:55:09 +0100

    an add weitergearbeite aber ist leider ein Fehler

--- src/app/projekt-list/projekt-hinzu/projekt-hinzu.component.ts
+++ src/app/projekt-list/projekt-hinzu/projekt-hinzu.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, ElementRef, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import {item} from '../../entities/projekt';
 
 @Component({
   selector: 'app-projekt-hinzu',
@@ -7,13 +8,40 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./projekt-hinzu.component.scss']
 })
 export class ProjektHinzuComponent implements OnInit {
+  @ViewChild('bearbInput')
+  bearbInputRef!: ElementRef;
+  @ViewChild('nameInput')
+  nameInputRef!: ElementRef;
+  @ViewChild('kundeInput')
+  kundeInputRef!: ElementRef;
+  @ViewChild('holzartInput')
+  holzartInputRef!: ElementRef;
+  @ViewChild('fraechterInput')
+  fraechterInputRef!: ElementRef;
+  @ViewChild('schaetzmeterInput')
+  schaetzmeterInputRef!: ElementRef;
+  @ViewChild('preisInput')
+  preisInputRef!: ElementRef;
+  @Output() hinzufuegen = new EventEmitter<item>();
 
-  ident = Date.now();
+  public ident:number = Date.now();
 
   constructor() { }
 
   ngOnInit():void {
 
+  }
+  onAddItem() {
+    const ingBearb = this.bearbInputRef.nativeElement.value;
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingKunde = this.kundeInputRef.nativeElement.value;
+    const ingholz = this.holzartInputRef.nativeElement.value;
+    const ingfr = this.fraechterInputRef.nativeElement.value;
+    const ingschaetz = this.schaetzmeterInputRef.nativeElement.value;
+    const ingpreis = this.preisInputRef.nativeElement.value;
+
+    const newProj:any = new item(this.ident,ingBearb,ingName,ingKunde,ingholz,ingfr,ingschaetz,ingpreis);
+    this.hinzufuegen.emit(newProj);
   }
 
 }
