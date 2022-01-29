94d338197446f7b7ef2d71b9895d6651c678d2c8
Thomas Haselwander <thomas.haselwander@gmail.com>
2022-01-29 07:44:10 +0100

    letzter stand

--- src/app/projekt-list/projekt-list.component.ts
+++ src/app/projekt-list/projekt-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { item } from '../entities/projekt';
-import { ProjektHinzuComponent } from './projekt-hinzu/projekt-hinzu.component';
+
 
 @Component({
   selector: 'app-projekt-list',
@@ -11,22 +12,25 @@ export class ProjektListComponent implements OnInit {
 
 
 
-  ident = Date.now();
 
   projekte: item[]=[
     new item(13012022,"Hans Bieber","Huber","PHA","FI","Mayer",250,95),
     new item(24012022,"Pieps Vogel","Franz","RHI","KI","Sepl",20,55),
     new item(26012022,"Karl Elefant","Huber","Heuer","FI","Mayer",7,85),
-    new item(26012022,"Josef Pingu","Hias","Mayer","EI","Sepl",15,450)
+    new item(26012022,"Josef Pingu","Hias","Mayer","EI","Sepl",15,450),
+
+
 
 
   ]
-  static projekte: any;
+
   constructor() {
 
   }
 
   ngOnInit(): void {
   }
-
+  onhinzufuegen(Item:item){
+    this.projekte.push(Item);
+  }
 }
