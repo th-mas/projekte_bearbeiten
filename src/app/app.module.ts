167ee2a1796944b99e94e05087f12a5143b26082
Thomas Haselwander <thomas.haselwander@gmail.com>
2022-01-30 13:26:18 +0100

    fas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,22 @@ import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {APP_ROUTES} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {RouterModule} from '@angular/router';
-import {ProjektListComponent} from './projekt-list/projekt-list.component';
-import {HeaderComponent} from './header/header.component';
-import {LandingpageComponent} from './landingpage/landingpage.component';
-import {ProjektSucheComponent} from './projekt-suche/projekt-suche.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {CommonModule} from '@angular/common';
-import {TimeTrackingModule} from "./time-tracking/time-tracking.module";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {GroupeComponent} from './groupe/groupe.component';
-import {UserComponent} from './user/user.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {LogInComponent} from './components/log-in/log-in.component';
-import {RegisterComponent} from './components/register/register.component';
-import {AngularMaterialModule} from './angular-material.module';
-import {IdentifyModule} from "./identify/identify.module";
-import {SorryModule} from "./sorry/sorry.module";
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ProjektListComponent } from './projekt-list/projekt-list.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { ProjektSucheComponent } from './projekt-suche/projekt-suche.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { CommonModule } from '@angular/common';
+import { ProjektHinzuComponent } from './projekt-list/projekt-hinzu/projekt-hinzu.component';
+import { FormsModule } from "@angular/forms";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+
+import { ProjektDeleteComponent } from './projekt-list/projekt-delete/projekt-delete.component';
+
 
 
 //import {FormBuilder, FormGroup, Validators} from '@angular/forms';
