a172150a1930ca717050eb3b1fa078f2e3e1e3b9
Thomas Haselwander <thomas.haselwander@gmail.com>
2022-01-30 15:45:55 +0100

    merge problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,13 @@ import { CommonModule } from '@angular/common';
 import { ProjektHinzuComponent } from './projekt-list/projekt-hinzu/projekt-hinzu.component';
 import { FormsModule } from "@angular/forms";
 import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/button";
 
 import { ProjektDeleteComponent } from './projekt-list/projekt-delete/projekt-delete.component';
 import { AngularMaterialModule } from './angular-material.module';
-import { IdentifyModule } from './identify/identify.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SorryModule } from './sorry/sorry.module';
+
 import { TimeTrackingModule } from './time-tracking/time-tracking.module';
+import { MatButtonModule } from '@angular/material/button/button-module';
 
 
 
@@ -41,17 +40,17 @@ import { TimeTrackingModule } from './time-tracking/time-tracking.module';
     ProjektDeleteComponent
   ],
   imports: [
-    BrowserModule, FormsModule,MatButtonModule,MatCardModule,  //FormBuilder,FormGroup,Validators,
+    BrowserModule,
+    MatButtonModule,
+    MatCardModule,
     CommonModule,
     RouterModule.forRoot(APP_ROUTES),
-    BrowserAnimationsModule,
     TimeTrackingModule,
-    IdentifyModule,
-    SorryModule,
     FormsModule,
     AngularMaterialModule,
     ReactiveFormsModule
   ],
+
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
